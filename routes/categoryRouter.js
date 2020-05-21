5dd1f215d95db2d18815f3fe43c45842d1679c4a
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-21 01:58:11 +0000

    refactor

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const categoryController = require('../controllers/categoryController');
 
 const router = express.Router();
-// const userController=require('../controllers/userController');
 
 router.route('/')
   .post(categoryController.createCategory)
