34a26b314028f742ab4eee50c0fcb71af3f13a9d
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-22 12:57:29 +0000

    add query by prices

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,15 +1,56 @@
+/* eslint-disable object-curly-newline */
 const db = require('../models/index');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const { Op } = db.Sequelize;
+
 exports.getProducts = catchAsync(async (req, res) => {
-  const { page, size } = req.query;
+  console.log(req.query);
+  const { page, size, order, dir, categories, prices, priceu, priced } = req.query;
+
   const queryObj = {};
   if (page && size) {
     queryObj.offset = (parseInt(page, 10) - 1) * parseInt(size, 10);
     queryObj.limit = parseInt(size, 10);
   }
-
+  if (order && dir) {
+    queryObj.order = [[order, dir]];
+  }
+  if (categories) {
+    queryObj.include = [
+      {
+        model: db.Category,
+        as: 'categories',
+        attributes: ['id', 'name'],
+        where: {
+          [Op.or]: categories.split(',').map((el) => ({ id: parseInt(el, 10) })),
+        },
+      },
+    ];
+  }
+  if (prices || priced || priceu) {
+    if (prices) {
+      queryObj.where = {
+        price: {
+          [Op.between]: [parseInt(prices.split(',')[0], 10), parseInt(prices.split(',')[1], 10)],
+        },
+      };
+    } else if (priceu) {
+      queryObj.where = {
+        price: {
+          [Op.lte]: [parseInt(priceu, 10)],
+        },
+      };
+    } else if (priced) {
+      queryObj.where = {
+        price: {
+          [Op.gte]: [parseInt(priced, 10)],
+        },
+      };
+    }
+  }
+  console.log(queryObj);
   const products = await db.Product.findAll(queryObj);
 
   return res.status(200).json({
